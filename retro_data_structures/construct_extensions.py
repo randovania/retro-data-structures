d0218bc54e547e85157965b4f81efa41460e2b56
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-06-09 11:48:20 +0300

    Transparently decompress MREA

--- retro_data_structures/construct_extensions.py
+++ retro_data_structures/construct_extensions.py
@@ -1,9 +1,15 @@
-from construct import FocusedSeq, Rebuild, this, len_, GreedyRange, Int32ul, stream_tell, Int32ub
+from typing import Any
+
+from construct import (
+    FocusedSeq, Rebuild, this, len_, GreedyRange, Int32ul, stream_tell, Int32ub, ListContainer,
+    EnumIntegerString, Container
+)
 
 
 def PrefixedArrayWithExtra(countfield, extrafield, subcon):
     r"""
-    Prefixes an array with item count (as opposed to prefixed by byte count, see :class:`~construct.core.Prefixed`), with an extra field between the count and the data.
+    Prefixes an array with item count (as opposed to prefixed by byte count, see :class:`~construct.core.Prefixed`),
+    with an extra field between the count and the data.
 
     Example::
 
@@ -16,16 +22,17 @@ def PrefixedArrayWithExtra(countfield, extrafield, subcon):
         [1684234849, 1751606885]
     """
     macro = FocusedSeq("items",
-        "count" / Rebuild(countfield, len_(this.items)),
-        "extra" / extrafield,
-        "items" / subcon[this.count],
-    )
+                       "count" / Rebuild(countfield, len_(this.items)),
+                       "extra" / extrafield,
+                       "items" / subcon[this.count],
+                       )
 
     def _actualsize(self, stream, context, path):
         position1 = stream_tell(stream, path)
         count = countfield._parse(stream, context, path)
         position2 = stream_tell(stream, path)
-        return (position2-position1) + count * subcon._sizeof(context, path) + extrafield._sizeof(context, path)
+        return (position2 - position1) + count * subcon._sizeof(context, path) + extrafield._sizeof(context, path)
+
     macro._actualsize = _actualsize
 
     def _emitseq(ksy, bitwise):
@@ -34,6 +41,27 @@ def PrefixedArrayWithExtra(countfield, extrafield, subcon):
             dict(id="extra", type=extrafield._compileprimitivetype(ksy, bitwise)),
             dict(id="data", type=subcon._compileprimitivetype(ksy, bitwise), repeat="expr", repeat_expr="countfield"),
         ]
+
     macro._emitseq = _emitseq
 
     return macro
+
+
+def convert_to_raw_python(value) -> Any:
+    if isinstance(value, ListContainer):
+        return [
+            convert_to_raw_python(item)
+            for item in value
+        ]
+
+    if isinstance(value, Container):
+        return {
+            key: convert_to_raw_python(item)
+            for key, item in value.items()
+            if not key.startswith("_")
+        }
+
+    if isinstance(value, EnumIntegerString):
+        return str(value)
+
+    return value
--- retro_data_structures/mrea.py
+++ retro_data_structures/mrea.py
@@ -1,14 +1,17 @@
 """
 Wiki: https://wiki.axiodl.com/w/MREA_(Metroid_Prime_2)
 """
+import hashlib
 import math
 import sys
 
 import construct
 import lzokay
 from construct import (
-    Int32ub, Struct, Const, Float32b, Array, Aligned, Int16sb
+    Int32ub, Struct, Const, Float32b, Array, Aligned, Int16sb, Computed
 )
+
+from retro_data_structures import construct_extensions
 
 
 class LZOSegment(construct.Construct):
@@ -25,57 +28,60 @@ class LZOSegment(construct.Construct):
             return lzokay.decompress(data, self.segment_size)
 
 
-class MaybeCompressedBytes(construct.Construct):
-    def length(self, context):
-        block = context.compressed_blocks[context._index]
-        if block.compressed_size == 0:
-            return block.uncompressed_size, False
-        else:
-            return block.compressed_size, True
+class LZOCompressedBlock(construct.Construct):
+    def __init__(self, compressed_size, uncompressed_size):
+        super().__init__()
+        self.compressed_size = compressed_size
+        self.uncompressed_size = uncompressed_size
 
     def _parse(self, stream, context, path):
-        length, compressed = self.length(context)
+        start_offset = 32 - (self.compressed_size % 32)
+        if start_offset != 32:
+            construct.stream_read(stream, start_offset, path)
 
-        if compressed:
-            start_offset = 32 - (length % 32)
-            if start_offset != 32:
-                construct.stream_read(stream, start_offset, path)
+        num_segments = self.uncompressed_size / 0x4000
+        size_left = self.uncompressed_size
+        segments = []
+        for _ in range(math.ceil(num_segments)):
+            new_segment = LZOSegment(min(0x4000, size_left))._parsereport(stream, context, path)
+            size_left -= len(new_segment)
+            segments.append(new_segment)
 
-        if compressed:
-            block = context.compressed_blocks[context._index]
-            num_segments = block.uncompressed_size / 0x4000
+        assert size_left == 0
+        return b"".join(segments)
 
-            size_left = block.uncompressed_size
-            segments = []
-            for _ in range(math.ceil(num_segments)):
-                new_segment = LZOSegment(min(0x4000, size_left))._parsereport(stream, context, path)
-                size_left -= len(new_segment)
-                segments.append(new_segment)
 
-            assert size_left == 0
+class CompressedBlocks(construct.Construct):
+    def __init__(self, count):
+        super().__init__()
+        self.count = count
 
-            return b"".join(segments)
-        else:
-            return construct.stream_read(stream, length, path)
+    def _parse(self, stream, context, path):
+        count = construct.evaluate(self.count, context)
 
-    def _build(self, obj, stream, context, path):
-        length, compressed = self.length(context)
+        headers = Array(count, Struct(
+            "buffer_size" / Int32ub,
+            "uncompressed_size" / Int32ub,
+            "compressed_size" / Int32ub,
+            "data_section_count" / Int32ub,
+        ))._parsereport(stream, context, path)
 
-        if compressed:
-            start_offset = 32 - (length % 32)
-            if start_offset != 32:
-                construct.stream_write(stream, b"\x00" * start_offset, start_offset, path)
+        obj = construct.ListContainer()
+        for block in headers:
+            if block.compressed_size == 0:
+                item = construct.stream_read(stream, block.uncompressed_size, path)
+            else:
+                item = LZOCompressedBlock(
+                    block.compressed_size,
+                    block.uncompressed_size
+                )._parsereport(stream, context, path)
 
-        data = construct.integer2bytes(obj, length) if isinstance(obj, int) else obj
-        data = bytes(data) if type(data) is bytearray else data
-        construct.stream_write(stream, data, length, path)
-        return data
+            e = construct.Container()
+            e["header"] = block
+            e["data"] = item
+            obj.append(e)
 
-    def _sizeof(self, context, path):
-        try:
-            return self.length(context)[0]
-        except (KeyError, AttributeError):
-            raise construct.SizeofError("cannot calculate size, key not found in context", path=path)
+        return obj
 
 
 def create(version: int, asset_id):
@@ -135,15 +141,20 @@ def create(version: int, asset_id):
         # Array containing the size of each data section in the file. Every size is always a multiple of 32.
         "data_section_sizes" / Aligned(32, Array(construct.this.data_section_count, Int32ub)),
 
-        "compressed_blocks" / Aligned(32, Array(construct.this.compressed_block_count, Struct(
-            "buffer_size" / Int32ub,
-            "uncompressed_size" / Int32ub,
-            "compressed_size" / Int32ub,
-            "data_section_count" / Int32ub,
-        ))),
-
-        "compressed_bytes" / Array(construct.this.compressed_block_count, MaybeCompressedBytes()),
+        "compressed_blocks" / Aligned(32, CompressedBlocks(construct.this.compressed_block_count)),
     ]
+
+    # And now the computed fields
+
+    def data_section(this):
+        index = this["_index"]
+        initial_offset = sum(this.data_section_sizes[:index])
+        return this.uncompressed_data[initial_offset:initial_offset + this.data_section_sizes[this["_index"]]]
+
+    fields.extend([
+        "uncompressed_data" / Computed(lambda this: b"".join(block.data for block in this.compressed_blocks)),
+        "data_sections" / Array(construct.this.data_section_count, Computed(data_section)),
+    ])
 
     return Struct(*fields)
 
@@ -152,8 +163,16 @@ Prime2MREA = create(0x19, Int32ub)
 
 
 def main():
+    import pprint
     mrea = Prime2MREA.parse_file(sys.argv[1])
-    print(mrea)
+    d = construct_extensions.convert_to_raw_python(mrea)
+
+    for block in d["compressed_blocks"]:
+        block["data_hash"] = hashlib.sha256(block.pop("data")).hexdigest()
+
+    d["uncompressed_data"] = hashlib.sha256(d["uncompressed_data"]).hexdigest()
+    d["data_sections"] = [hashlib.sha256(it).hexdigest() for it in d["data_sections"]]
+    pprint.pprint(d)
 
 
 if __name__ == '__main__':
