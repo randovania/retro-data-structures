416bb51ee831fc83f35203d5315b7e59f74a0a66
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-06-16 00:41:49 +0300

    PAK: calculate size and offset. Build from a simple API.

--- retro_data_structures/pak.py
+++ retro_data_structures/pak.py
@@ -1,11 +1,12 @@
 import construct
 from construct import (Struct, Const, Int16ub, PrefixedArray, Int32ub, PascalString, IfThenElse,
-                       FocusedSeq, Bytes, Lazy, Pointer, Prefixed, Aligned, FixedSized, Byte, Tell, Rebuild,
-                       GreedyBytes, Pass, Computed)
+                       FocusedSeq, Pointer, Aligned, Tell, Rebuild,
+                       GreedyBytes, Array, Seek, Computed, RawCopy, Prefixed)
 
 from retro_data_structures import game_check
-from retro_data_structures.common_types import FourCC, AssetId32, AssetId64
+from retro_data_structures.common_types import ObjectTag_32
 from retro_data_structures.compression import LZOCompressedBlock, ZlibCompressedBlock
+from retro_data_structures.construct_extensions import AlignTo, AlignedPrefixed
 
 PAKHeader = Struct(
     version_major=Const(3, Int16ub),
@@ -13,56 +14,91 @@ PAKHeader = Struct(
     unused=Const(0, Int32ub),
 )
 
+ResourceHeader = Struct(
+    compressed=Int32ub,
+    asset=ObjectTag_32,
+    size=Int32ub,
+    offset=Int32ub,
+)
 
-def create(asset_id):
+
+def header_field(offset):
+    def result(ctx):
+        parents = [ctx]
+        while "_" in parents[-1]:
+            parents.append(parents[-1]["_"])
+
+        start_headers = None
+        index = None
+
+        for c in reversed(parents):
+            if "_start_headers" in c:
+                start_headers = c["_start_headers"]
+                break
+
+        for c in parents:
+            if "_resource_index" in c:
+                index = c["_resource_index"]
+                break
+
+        if index is None or start_headers is None:
+            raise ValueError("Missing required context key")
+
+        return start_headers + (index * ResourceHeader.sizeof()) + offset
+
+    return result
+
+
+def skip_headers(ctx):
+    result = ResourceHeader.sizeof() * ctx["_num_resources"]
+    return result
+
+
+CompressedPakResource = FocusedSeq(
+    "data",
+    decompressed_size=Rebuild(Int32ub, construct.len_(construct.this.data)),
+    data=IfThenElse(
+        game_check.uses_lzo,
+        LZOCompressedBlock(construct.this.decompressed_size),
+        ZlibCompressedBlock,
+    ),
+)
+
+
+def create():
     return "PAK" / Struct(
-        header=PAKHeader,
+        _header=PAKHeader,
         named_resources=PrefixedArray(Int32ub, Struct(
-            asset_type=FourCC,
-            asset_id=asset_id,
+            asset=ObjectTag_32,
             name=PascalString(Int32ub, "utf-8"),
         )),
-        resources=PrefixedArray(Int32ub, Struct(
-            compressed=Int32ub,
-            asset_type=FourCC,
-            asset_id=asset_id,
-            size=FocusedSeq("address", address=Tell, value=construct.Seek(4, 1)),
-            offset=Int32ub,
-            data=Lazy(Pointer(
-                construct.this.offset,
-                IfThenElse(
-                    construct.this.compressed > 0,
-                    Struct(
-                        # "data",
-                        decompressed_size=Rebuild(Int32ub, construct.len_(construct.this.data)),
-                        compressed_size=Pointer(construct.this._.size, Int32ub),
-                        data=Prefixed(
-                            Pointer(construct.this._.size, Int32ub),
-                            Aligned(32, IfThenElse(
-                                game_check.uses_lzo,
-                                LZOCompressedBlock(construct.this.decompressed_size),
-                                ZlibCompressedBlock,
-                            ))
-                        ),
+        _num_resources=Rebuild(Int32ub, construct.len_(construct.this.resources)),
+        _start_headers=Tell,
+        _skip_headers=Seek(skip_headers, 1),
+        _align=AlignTo(32),
+        resources=Array(
+            construct.this["_num_resources"],
+            Aligned(32, Struct(
+                _start=Tell,
+                _resource_index=Computed(lambda ctx: ctx["_index"]),
+                compressed=Pointer(header_field(0x0), Int32ub),
+                asset=Pointer(header_field(0x4), ObjectTag_32),
+                contents=RawCopy(Prefixed(
+                    Pointer(header_field(0xC), Int32ub),
+                    IfThenElse(
+                        construct.this.compressed > 0,
+                        CompressedPakResource,
+                        GreedyBytes,
                     ),
-                    Struct(
-                        data=Prefixed(
-                            Pointer(construct.this._.size, Int32ub),
-                            GreedyBytes,
-                        ),
-                        compressed_size=Computed(construct.len_(construct.this.data)),
-                    )
-                ),
+                    # 32,
+                    # Int32ub.length,
+                )),
+                _end=Tell,
+                size=Pointer(header_field(0xC), Rebuild(Int32ub, construct.this.contents.length)),
+                _offset=Pointer(header_field(0x10), Rebuild(Int32ub, lambda ctx: ctx["_start"])),
             )),
-        )),
+        ),
     )
 
 
-PAK_AssetId32 = create(AssetId32)
-PAK_AssetId64 = create(AssetId64)
-
-PAK = IfThenElse(
-    game_check.uses_asset_id_32,
-    PAK_AssetId32,
-    PAK_AssetId64,
-)
+PAK = create()
