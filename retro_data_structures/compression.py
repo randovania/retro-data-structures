e553b85a7bddbf373e56c91f63b3c02dabc44cc7
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-06-17 22:24:21 +0300

    Fix building LZOCompressedBlock/LZOSegment

--- retro_data_structures/compression.py
+++ retro_data_structures/compression.py
@@ -3,7 +3,7 @@ import math
 
 import construct
 import lzokay
-from construct import GreedyRange, ExprAdapter
+from construct import GreedyRange, Adapter
 
 
 class CompressedLZO(construct.Tunnel):
@@ -26,10 +26,16 @@ class LZOSegment(construct.Subconstruct):
         self.decompressed_size = decompressed_size
 
     def _parse(self, stream, context, path):
+        decompressed_size = construct.evaluate(self.decompressed_size, context)
+
         length = construct.Int16sb._parsereport(stream, context, path)
         data = construct.stream_read(stream, abs(length), path)
         if length > 0:
-            return self.subcon._parsereport(io.BytesIO(data), context, path)
+            result = self.subcon._parsereport(io.BytesIO(data), context, path)
+            if len(result) != decompressed_size:
+                raise construct.StreamError(f"Expected to decompress {decompressed_size} bytes, got {len(result)}",
+                                            path)
+            return result
         else:
             return data
 
@@ -38,6 +44,7 @@ class LZOSegment(construct.Subconstruct):
         buildret = self.subcon._build(uncompressed, stream2, context, path)
         compressed_data = stream2.getvalue()
         if len(compressed_data) < len(uncompressed):
+            print("COMPRESSING!", len(compressed_data))
             construct.Int16sb._build(len(compressed_data), stream, context, path)
             construct.stream_write(stream, compressed_data, len(compressed_data), path)
             return buildret
@@ -47,13 +54,38 @@ class LZOSegment(construct.Subconstruct):
             return buildret
 
 
-def LZOCompressedBlock(decompressed_size, segment_size=0x4000):
-    return ExprAdapter(
-        GreedyRange(LZOSegment(segment_size)),
-        lambda segments, ctx: b"".join(segments),
-        lambda uncompressed, ctx: [uncompressed[segment_size * i:segment_size * (i + 1)]
-                                   for i in range(math.ceil(len(uncompressed) / segment_size))]
-    )
+class LZOCompressedBlock(Adapter):
+    def __init__(self, decompressed_size, segment_size=0x4000):
+        super().__init__(GreedyRange(LZOSegment(self._actual_segment_size)))
+        self.decompressed_size = decompressed_size
+        self.segment_size = segment_size
+
+    def _actual_segment_size(self, context):
+        decompressed_size = construct.evaluate(self.decompressed_size, context)
+        segment_size = construct.evaluate(self.segment_size, context)
+
+        previous_segments = context._index * segment_size
+        if previous_segments > decompressed_size:
+            # This segment is redundant!
+            raise construct.StopFieldError()
+
+        elif previous_segments + segment_size > decompressed_size:
+            # Last segment
+            return decompressed_size - previous_segments
+
+        else:
+            # Another segment with this size
+            return segment_size
+
+    def _decode(self, segments, context, path):
+        return b"".join(segments)
+
+    def _encode(self, uncompressed, context, path):
+        segment_size = self.segment_size
+        return [
+            uncompressed[segment_size * i:segment_size * (i + 1)]
+            for i in range(math.ceil(len(uncompressed) / segment_size))
+        ]
 
 
 ZlibCompressedBlock = construct.Compressed(construct.GreedyBytes, "zlib", level=9)
