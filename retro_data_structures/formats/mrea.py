d18849109f4df00016e246e53884abb2107de01e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-08-08 01:24:55 +0300

    Fix decoding Echoes MREA

--- retro_data_structures/formats/mrea.py
+++ retro_data_structures/formats/mrea.py
@@ -6,13 +6,14 @@ import io
 
 import construct
 from construct import (
-    Int32ub, Struct, Const, Float32b, Array, Aligned, Computed, Switch, Peek, FocusedSeq, Sequence, IfThenElse,
-    Prefixed, GreedyBytes, Adapter, ListContainer, Container, Tell, Rebuild, Pointer
+    Int32ub, Struct, Const, Float32b, Array, Aligned, GreedyBytes, ListContainer, Container, Rebuild,
+    Tell, Computed, FocusedSeq, IfThenElse, Prefixed, Pointer, Subconstruct, Switch
 )
 
+from retro_data_structures import game_check
 from retro_data_structures.common_types import AssetId32
 from retro_data_structures.compression import LZOCompressedBlock
-from retro_data_structures.data_section import DataSectionSizes
+from retro_data_structures.data_section import DataSectionSizes, DataSectionSizePointer
 
 
 class PrefixedWithPaddingBefore(construct.Subconstruct):
@@ -44,6 +45,41 @@ class PrefixedWithPaddingBefore(construct.Subconstruct):
 
         construct.stream_write(stream, data, len(data), path)
         return buildret
+
+
+class DataSectionInGroup(Subconstruct):
+    def _parse(self, stream, context, path):
+        group = self.subcon._parsereport(stream, context, path)
+
+        size_pointer = DataSectionSizePointer()
+
+        sections = []
+        offset = 0
+        for i in range(group.section_count):
+            section_size = size_pointer._parsereport(stream, context, path)
+            data = group.data[offset:offset + section_size]
+
+            sections.append(Container(
+                data=data,
+                hash=hashlib.sha256(data).hexdigest(),
+            ))
+            offset += section_size
+
+        return Container(
+            compressed=group.header.value.compressed_size > 0,
+            sections=ListContainer(sections),
+        )
+
+    def _build(self, sections, stream, context, path):
+        if sections:
+            raise NotImplementedError
+
+        compressed_blocks = []
+        obj2 = ListContainer(compressed_blocks)
+
+        buildret = self.subcon._build(obj2, stream, context, path)
+
+        return obj2
 
 
 def create(version: int, asset_id):
@@ -101,34 +137,10 @@ def create(version: int, asset_id):
         "_compressed_block_count" / Aligned(16, Rebuild(Int32ub, construct.len_(construct.this.section_groups))),
 
         # Array containing the size of each data section in the file. Every size is always a multiple of 32.
-        "_data_section_sizes" / Aligned(32, DataSectionSizes(construct.this.data_section_count)),
-    ]
+        "_data_section_sizes" / Aligned(32, DataSectionSizes(construct.this._root.data_section_count)),
+        "_current_section" / construct.Computed(lambda this: 0),
 
-    class DataSectionInGroup(Adapter):
-        def _decode(self, group, context, path):
-            if "section_count_offset" not in context:
-                context["section_count_offset"] = 0
-
-            sections = []
-            offset = 0
-            for i in range(group.section_count):
-                section_size = context._._data_section_sizes[context.section_count_offset + i]
-                sections.append(Container(data=group.data[offset:offset + section_size.value]))
-                offset += section_size.value
-
-            context.section_count_offset += group.section_count
-
-            return ListContainer(sections)
-
-        def _encode(self, sections, context, path):
-            if sections:
-                raise NotImplementedError
-
-            compressed_blocks = []
-
-            return ListContainer(compressed_blocks)
-
-    fields.extend([
+        # Sections. Each group is compressed separately
         "section_groups" / Aligned(32, FocusedSeq(
             "groups",
             headers=Array(construct.this._._compressed_block_count, Struct(
@@ -143,36 +155,31 @@ def create(version: int, asset_id):
             groups=Array(
                 construct.this._._compressed_block_count,
                 DataSectionInGroup(Struct(
-                    _header=Computed(lambda this: this._.headers[this._index]),
-                    section_count=Pointer(lambda this: this._header.address + 12, Int32ub),
+                    header=Computed(lambda this: this._.headers[this._index]),
+                    section_count=Pointer(lambda this: this.header.address + 12, Int32ub),
                     data=IfThenElse(
-                        construct.this._header.value.compressed_size == 0,
-                        Prefixed(Pointer(lambda this: this._header.address + 4, Int32ub), GreedyBytes),
-                        PrefixedWithPaddingBefore(Computed(lambda this: this._header.value.compressed_size),
-                                                  LZOCompressedBlock(0x0),
-                                                  ),
-                    ),
+                        lambda this: this.header.value.compressed_size > 0,
+                        PrefixedWithPaddingBefore(
+                            Computed(lambda this: this.header.value.compressed_size),
+                            LZOCompressedBlock(lambda this: this.header.value.uncompressed_size),
+                        ),
 
-                    # Adding some hash to easy validation
-                    hash=Computed(lambda this: hashlib.sha256(this.data).hexdigest())
+                        Prefixed(Pointer(lambda this: this.header.address + 4, Int32ub), GreedyBytes),
+                    ),
                 )),
-            )
+            ),
         )),
-    ])
+    ]
 
     return Struct(*fields)
 
 
 Prime2MREA = create(0x19, AssetId32)
 
-MREA = FocusedSeq(
-    "mrea",
-    header=Peek(Sequence(Int32ub, Int32ub)),
-    mrea=Switch(
-        construct.this.header[1],
-        {
-            0x19: Prime2MREA,
-        },
-        construct.Error,
-    )
+MREA = Switch(
+    game_check.get_current_game,
+    {
+        game_check.Game.ECHOES: Prime2MREA,
+    },
+    construct.Error,
 )
