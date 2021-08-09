f8e9fd85e0aaa866a4477d05f3a8a106db2bb3f3
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-08-10 01:36:41 +0300

    Somewhat friendlier API for properties

--- retro_data_structures/formats/scly.py
+++ retro_data_structures/formats/scly.py
@@ -1,61 +1,6 @@
-import construct
-from construct import (Struct, Const, Int8ub, Int32ub, PrefixedArray, Int16ub, PaddedString, Hex,
-                       Switch, Bytes, Flag)
+from construct import (Struct, Const, Int8ub, Int32ub, PrefixedArray)
 
-from retro_data_structures.common_types import FourCC, String
-
-PROPERTY_TYPES = {}
-
-Property = Struct(
-    id=Hex(Int32ub),
-    size=Int16ub,
-    data=Switch(
-        construct.this.id,
-        PROPERTY_TYPES,
-        Bytes(construct.this.size),
-    )
-)
-
-SubProperties = PrefixedArray(Int16ub, Property)
-
-PROPERTY_TYPES.update({
-    0x255A4580: SubProperties,
-    0x494E414D: String,
-    0x41435456: Flag,
-    0xB581574B: Int32ub,
-    0x95F8D644: Int32ub,
-    0x3FA164BC: Int32ub,
-    0xDE3E40A3: Int32ub,
-    0xD3AF8D72: Int32ub,
-    0x8DB9398A: Int32ub,
-    0x03BDEA98: Int32ub,
-    0x70729364: Int32ub,
-    0xCEC16932: SubProperties,
-    0xE709DDC0: SubProperties,
-    0x49614C51: SubProperties,
-    0xB498B424: SubProperties,
-})
-
-
-PropertyStruct = Struct(
-    id=Hex(Int32ub),
-    size=Int16ub,
-    data=SubProperties,
-)
-
-Connection = Struct(
-    state=PaddedString(4, "ascii"),
-    message=PaddedString(4, "ascii"),
-    target=Hex(Int32ub),
-)
-
-ScriptInstance = Struct(
-    type=FourCC,
-    size=Int16ub,  # TODO: calculate. Size in bytes starting after this field.
-    id=Hex(Int32ub),
-    connections=PrefixedArray(Int16ub, Connection),
-    properties=PropertyStruct,
-)
+from retro_data_structures.formats.script_object import ScriptInstance
 
 SCLY = Struct(
     magic=Const(b"SCLY"),
