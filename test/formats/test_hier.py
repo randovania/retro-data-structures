981803b9af2714843fcc41361ee93f5902b0d6b9
duncathan salt <duncathan@duncathan.com>
2021-10-01 14:25:47 -0600

    Adds HIER support (#3)
    * add HIER
    
    * add hier test
    
    * fixes hier test
    
    Co-authored-by: duncathan <dunc@duncathan.com>

---
+++ test/formats/test_hier.py
@@ -0,0 +1,8 @@
+from retro_data_structures.formats.hier import HIER
+from retro_data_structures.game_check import Game
+from test.test_lib import parse_and_build_compare
+
+def test_compare(prime2_pwe_project):
+    parse_and_build_compare(HIER, Game.ECHOES, prime2_pwe_project.joinpath(
+        "Resources/NoARAM/DUMB_ScanHierarchy.DUMB"
+    ))
