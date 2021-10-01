6ca070e070d4827157947615428e95d29d29b6ce
duncathan <dunc@duncathan.com>
2021-09-30 19:20:59 -0600

    STRG corrections

--- retro_data_structures/formats/strg.py
+++ retro_data_structures/formats/strg.py
@@ -1,5 +1,9 @@
+"""
+https://wiki.axiodl.com/w/STRG_(Metroid_Prime)
+"""
+
 import construct
-from construct import (Struct, Const, Int32ub, Array, CString, Rebuild, Computed, If, IfThenElse)
+from construct import (this, len_, Enum, Struct, Const, Int32ub, Array, CString, Rebuild, Computed, If, IfThenElse)
 
 from retro_data_structures.common_types import (FourCC, String)
 from retro_data_structures import game_check
@@ -9,38 +13,38 @@ Language = Struct(
     offset=Rebuild(
         Int32ub,
         IfThenElse(
-            construct.this._index == 0,
+            this._index == 0,
             Const(0, Int32ub),
-            Computed(construct.this._[construct.this._index-1].offset + construct.this._[construct.this._index-1].size)
+            Computed(this._.language_table[this._index-1].offset + this._.language_table[this._index-1].size)
         )
     ),
     size=If(
         game_check.is_prime2,
         Rebuild(
             Int32ub,
-            construct.Construct.sizeof(construct.this._.string_tables[construct.this._index]),
+            this._.string_tables[this._index].sizeof(),
         )
     )
 )
 
 NameTable = Struct(
-    name_count=Rebuild(Int32ub, construct.len_(construct.this.name_entries)),
-    size=Rebuild(Int32ub, Computed(construct.Construct.sizeof(construct.this.name_entries) + construct.Construct.sizeof(construct.this.name_strings))),
+    name_count=Rebuild(Int32ub, len_(this.name_entries)),
+    size=Rebuild(Int32ub, Computed(this.name_entries.sizeof() + this.name_array.sizeof())),
     name_entries=Array(
-        construct.this.name_count,
+        this.name_count,
         Struct(
             offset=Rebuild(
                 Int32ub,
                 IfThenElse(
-                    construct.this._index == 0,
-                    construct.Construct.sizeof(construct.this._),
-                    Computed(construct.this._[construct.this._index-1].offset + construct.Construct.sizeof(construct.this._._.name_array[construct.this._index-1]))
+                    this._index == 0,
+                    this._.name_entries.sizeof(),
+                    Computed(this._.name_entries[this._index-1].offset + this._.name_array[this._index-1].sizeof())
                 )
             ),
             index=Int32ub,
         )
     ),
-    name_array=Array(construct.this.name_count, String)
+    name_array=Array(this.name_count, String)
 )
 
 StringTable = Struct(
@@ -48,29 +52,29 @@ StringTable = Struct(
         game_check.is_prime1,
         Rebuild(
             Int32ub,
-            Computed(construct.Construct.sizeof(construct.this._) - construct.Construct.sizeof(construct.this))
+            Computed(this.sizeof() - construct.Construct.sizeof(Int32ub))
         )
     ),
     offsets=Array(
-        construct.this._.string_count,
+        this._.string_count,
         Rebuild(
             Int32ub,
             IfThenElse(
-                construct.this._index == 0,
+                this._index == 0,
                 Const(0, Int32ub),
-                Computed(construct.this._[construct.this._index-1] + construct.Construct.sizeof(construct.this._._.strings[construct.this._index-1]))
+                Computed(this.offsets[this._index-1] + this.strings[this._index-1].sizeof())
             )
         )
     ),
-    strings=Array(construct.this._.string_count, CString("utf-16"))
+    strings=Array(this._.string_count, CString("utf-16"))
 )
 
 STRG = Struct(
     magic=Const(0x87654321, Int32ub),
-    version=Int32ub,
-    language_count=Rebuild(Int32ub, construct.len_(construct.this.language_table)),
-    string_count=Rebuild(Int32ub, construct.len_(construct.this.string_tables[0])),
-    language_table=Array(construct.this.language_count, Language),
+    version=Enum(Int32ub, prime1=0, prime2=1, prime3=3),
+    language_count=Rebuild(Int32ub, len_(this.language_table)),
+    string_count=Rebuild(Int32ub, len_(this.string_tables[0])),
+    language_table=Array(this.language_count, Language),
     name_table=If(game_check.is_prime2, NameTable),
-    string_tables=Array(construct.this.language_count, StringTable)
+    string_tables=Array(this.language_count, StringTable)
 )
