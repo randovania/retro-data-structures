f3660631eb8adc25daed4e3763a2b3e240501d2c
duncathan <dunc@duncathan.com>
2021-10-02 04:43:21 -0600

    add scan properties

--- retro_data_structures/properties.py
+++ retro_data_structures/properties.py
@@ -38,3 +38,32 @@ AddPropertyInfo(0x49614C51, SubProperties)
 AddPropertyInfo(0xB498B424, SubProperties)
 AddPropertyInfo(0xFFFFFFFF, SubProperties, "Base property struct")
 
+# ScannableObjectInfo Properties
+AddPropertyInfo(0x2F5B6423, AssetIdCorrect, "STRG, Scan Text")
+AddPropertyInfo(0xC308A322, Int32sb, "Scan Speed (0 for fast, 1 for slow)")
+AddPropertyInfo(0x7B714814, Flag, "Is Important? (0 for blue, 1 for red)")
+AddPropertyInfo(0x1733B1EC, Flag, "Use Logbook Model After Scan?")
+AddPropertyInfo(0x53336141, AssetIdCorrect, "CMDL, Post-Scan Override Texture")
+AddPropertyInfo(0x3DE0BA64, Float32b, "Logbook Default X Rotation")
+AddPropertyInfo(0x2ADD6628, Float32b, "Logbook Default Z Rotation")
+AddPropertyInfo(0xD0C15066, Float32b, "Logbook Scale")
+AddPropertyInfo(0xB7ADC418, AssetIdCorrect, "CMDL, Logbook Model")
+AddPropertyInfo(0x15694EE1, Byte, "AnimationParameters, Logbook AnimSet")
+AddPropertyInfo(0x58F9FE99, Byte, "AnimationParameters, Unknown")
+AddPropertyInfo(0x1C5B4A3A, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x1C5B4A3A, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x8728A0EE, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0xF1CD99D3, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x6ABE7307, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x1C07EBA9, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x8774017D, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0xF1913840, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x6AE2D294, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+AddPropertyInfo(0x1CE2091C, SubProperties, "ScanInfoSecondaryModel, Secondary Model 1")
+# TODO: unknown prime 3 property 1
+# TODO: unknown prime 3 property 2
+
+# ScanInfoSecondaryModel Properties
+AddPropertyInfo(0x1F7921BC, AssetIdCorrect, "CMDL, Model")
+AddPropertyInfo(0xCDD202D1, Byte, "AnimationParameters, AnimSet")
+AddPropertyInfo(0x3EA2BED8, String, "Attach Bone Name")
