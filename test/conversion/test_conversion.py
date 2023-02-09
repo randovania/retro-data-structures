274e11eff2704b27fe019b643b7f16c40dd8ef5b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-02-09 17:05:56 +0200

    Fix tests

--- test/conversion/test_conversion.py
+++ test/conversion/test_conversion.py
@@ -6,7 +6,7 @@ from retro_data_structures.game_check import Game
 
 
 @pytest.mark.parametrize("asset_type", ["ANCS", "ANIM", "CINF", "CMDL", "CSKR", "EVNT", "PART", "TXTR"])
-@pytest.mark.parametrize("game", Game)
+@pytest.mark.parametrize("game", [Game.PRIME, Game.ECHOES, Game.CORRUPTION])
 def test_converter_for(asset_type, game: Game):
     details = AssetDetails(
         asset_id=None,
