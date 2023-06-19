62039a49496e59bb552fd850fd8f623eff9050e1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-06-19 14:41:26 +0300

    Improve EnumAdapter

--- retro_data_structures/adapters/enum_adapter.py
+++ retro_data_structures/adapters/enum_adapter.py
@@ -1,13 +1,18 @@
-from construct import Adapter, Enum, Int32ub
+import enum
+import typing
+
+import construct
+
+E = typing.TypeVar("E", bound=enum.IntEnum)
 
 
-class EnumAdapter(Adapter):
-    def __init__(self, enum_class, subcon=Int32ub):
-        super().__init__(Enum(subcon, enum_class))
+class EnumAdapter(construct.Adapter, typing.Generic[E]):
+    def __init__(self, enum_class: type[E], subcon=construct.Int32ub):
+        super().__init__(construct.Enum(subcon, enum_class))
         self._enum_class = enum_class
 
-    def _decode(self, obj, context, path):
+    def _decode(self, obj: str, context, path) -> E:
         return self._enum_class[obj]
 
-    def _encode(self, obj, context, path):
+    def _encode(self, obj: E, context, path) -> str:
         return obj.name
