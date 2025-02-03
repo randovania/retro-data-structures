8cd655db9e16fe30517369f7bed922d7d417ce68
xchellx <jon.xjonx.nat@gmail.com>
2025-02-03 17:13:48 -0500

    release branch for commit 5fa77535569ca5ef2c40d669167981c6c6cc4b92

--- install.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-IF EXIST requirements.txt python -m pip install -U -r requirements.txt
-python -m pip install .
--- venv.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-py -3 -m venv C:\git\retro-data-structures-venv
-CALL C:\git\retro-data-structures-venv\Scripts\activate.bat
