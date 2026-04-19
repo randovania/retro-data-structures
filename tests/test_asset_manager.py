4da98f72bcf62d54e341fb55cbff7f1bdb41be4a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-04-19 15:42:58 +0300

    Add test

--- tests/test_asset_manager.py
+++ tests/test_asset_manager.py
@@ -5,7 +5,8 @@ from typing import TYPE_CHECKING
 
 import nod
 
-from retro_data_structures.asset_manager import IsoFileProvider, PathFileProvider
+from retro_data_structures.asset_manager import AssetManager, IsoFileProvider, PathFileProvider
+from retro_data_structures.game_check import Game
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -26,3 +27,18 @@ def test_identical_provider_prime2(prime2_iso: Path, tmp_path: Path) -> None:
     path_provider = PathFileProvider(extract_path)
 
     assert sorted(iso_provider.rglob("*")) == sorted(path_provider.rglob("*"))
+
+
+def test_get_custom_name(prime2_iso_provider):
+    asset_manager = AssetManager(prime2_iso_provider, target_game=Game.ECHOES)
+
+    assert asset_manager.get_custom_asset("MyFancyAsset") is None
+    assert asset_manager.get_custom_name_for(0xFFFF0000) is None
+
+    asset_manager.register_custom_asset_name("MyFancyAsset", 0xFFFF0000)
+
+    assert asset_manager.get_custom_asset("MyFancyAsset") == 0xFFFF0000
+    assert asset_manager.get_custom_name_for(0xFFFF0000) == "MyFancyAsset"
+
+    assert asset_manager.get_custom_asset("MyUglyAsset") is None
+    assert asset_manager.get_custom_name_for(0xFFFFF000) is None
