fab0201629ac0b1528ebcbe3d4f264e868b9feee
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-05-01 17:37:15 +0300

    Improve test for SCAN

--- tests/formats/test_scan.py
+++ tests/formats/test_scan.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import typing
+
 import pytest
 from tests import test_lib
 
 from retro_data_structures.formats.scan import Scan
+from retro_data_structures.properties.echoes.objects import ScannableObjectInfo
 
 
 def test_compare_p1(prime1_asset_manager):
@@ -17,10 +20,21 @@ def test_compare_p1(prime1_asset_manager):
 
 def test_compare_p2(prime2_asset_manager):
     # Resources/Uncategorized/Brizgee_0.SCAN
-    test_lib.parse_and_build_compare(
+    _, obj, _ = test_lib.parse_and_build_compare(
         prime2_asset_manager,
         0x62572238,
         Scan,
+    )
+    scan = typing.cast("Scan", obj)
+    prop = scan.scannable_object_info.get_properties_as(ScannableObjectInfo)
+    strg = prime2_asset_manager.get_parsed_asset(prop.string)
+    assert strg.strings == (
+        "Morphology: Brizgee\nVenomous insectoid.",
+        "Poisoned stinger concealed in back-mounted shell. Finish it off quickly once the stinger is exposed.",
+        "The Brizgee's tail ends in a venomous barb, which it conceals underneath a "
+        "hard-packed layer of fused sand. A series of sharpened ridges along its body "
+        "discourages most predators; those foolish enough to harass the Brizgee are "
+        "quickly introduced to its deadly sting.",
     )
 
 
