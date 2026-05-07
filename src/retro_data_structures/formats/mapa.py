410d6bb611a0a71c4e95c58b643a9da28e318338
duncathan <dunc@duncathan.com>
2026-05-07 11:30:44 -0600

    new API for MAPA, and improve writing
    I've tested, and this fixes the crash I was encountering when adding map icons in OPR
    
    also drops support for 3.10 and 3.11

--- src/retro_data_structures/formats/mapa.py
+++ src/retro_data_structures/formats/mapa.py
@@ -11,12 +11,17 @@ from retro_data_structures.common_types import AABox, Transform4f, Vector3
 from retro_data_structures.construct_extensions.misc import ErrorWithMessage
 from retro_data_structures.game_check import Game, current_game_at_least_else, get_current_game
 
+if typing.TYPE_CHECKING:
+    from retro_data_structures.formats.script_object import InstanceId
+
+Vec3 = tuple[float, float, float]
+
 
 def _const(val: int):
     return construct.Const(val, construct.Int32ub)
 
 
-class AreaVisibilty(IntEnum):
+class AreaVisibility(IntEnum):
     Always = 0
     VisitOrMapStation = 1
     VisitOnly = 2
@@ -103,23 +108,135 @@ class GXPrimitive(IntEnum):
         return self.name
 
 
-MappableObject = construct.Struct(
+_MappableObjectConstruct = construct.Struct(
     type=construct.Switch(
         get_current_game,
         {
-            Game.PRIME: construct.Enum(construct.Int32sb, ObjectTypeMP1),
-            Game.ECHOES: construct.Enum(construct.Int32sb, ObjectTypeMP2),
+            Game.PRIME: EnumAdapter(ObjectTypeMP1),
+            Game.ECHOES: EnumAdapter(ObjectTypeMP2),
         },
         default=construct.Int32sb,
     ),
-    visibility_mode=construct.Enum(construct.Int32ub, ObjectVisibility),
+    visibility_mode=EnumAdapter(ObjectVisibility),
     editor_id=construct.Int32ub,
     unk1=construct.Int32ub,
     transform=Transform4f,
     unk2=construct.Int32ub[4],
 )
 
-Primitive = construct.Aligned(
+
+class MappableObject[T: (ObjectTypeMP1, ObjectTypeMP2)]:
+    def __init__(self, raw: construct.Container):
+        self._raw = raw
+
+    def __repr__(self) -> str:
+        object_type = self.object_type
+        visibility_mode = self.visibility_mode
+        editor_id = self.editor_id
+        transform = self.transform
+
+        return f"{self.__class__.__qualname__}({object_type=}, {visibility_mode=}, {editor_id=}, {transform=})"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, MappableObject):
+            return False
+
+        return (
+            self.object_type == other.object_type
+            and self.visibility_mode == other.visibility_mode
+            and self.editor_id == other.editor_id
+            and self.transform == other.transform
+        )
+
+    __hash__ = None
+
+    @classmethod
+    def create(
+        cls,
+        object_type: T,
+        visibility_mode: ObjectVisibility,
+        editor_id: InstanceId,
+        transform: list[float],
+    ) -> typing.Self:
+        obj = cls(
+            construct.Container(
+                {
+                    "unk1": 0xFFFFFFFF,
+                    "unk2": [0xFFFFFFFF] * 4,
+                }
+            )
+        )
+        obj.object_type = object_type
+        obj.visibility_mode = visibility_mode
+        obj.editor_id = editor_id
+        obj.transform = transform
+        return obj
+
+    @property
+    def object_type(self) -> T:
+        return self._raw.type
+
+    @object_type.setter
+    def object_type(self, value: T) -> None:
+        self._raw.type = value
+
+    @property
+    def visibility_mode(self) -> ObjectVisibility:
+        return self._raw.visibility_mode
+
+    @visibility_mode.setter
+    def visibility_mode(self, value: ObjectVisibility) -> None:
+        self._raw.visibility_mode = value
+
+    @property
+    def editor_id(self) -> InstanceId:
+        from retro_data_structures.formats.script_object import InstanceId
+
+        return InstanceId(self._raw.editor_id)
+
+    @editor_id.setter
+    def editor_id(self, value: InstanceId) -> None:
+        self._raw.editor_id = value
+
+    @property
+    def transform(self) -> list[float]:
+        return self._raw.transform
+
+    @transform.setter
+    def transform(self, value: list[float]):
+        if len(value) != 12:
+            raise ValueError("Invalid transform")
+        self._raw.transform = value
+
+
+class MappableObjectAdapter(construct.Adapter):
+    def __init__(self):
+        super().__init__(_MappableObjectConstruct)
+
+    def _decode(self, obj: construct.Container, context: construct.Container, path: str) -> MappableObject:
+        return MappableObject(obj)
+
+    def _encode(self, obj: MappableObject, context: construct.Container, path: str) -> construct.Container:
+        return obj._raw
+
+
+MappableObjectConstruct = MappableObjectAdapter()
+
+
+class Vec3TupleAdapter(construct.Adapter):
+    def __init__(self):
+        super().__init__(Vector3)
+
+    def _decode(self, obj: list[float], context: construct.Container, path: str) -> Vec3:
+        return tuple(obj)
+
+    def _encode(self, obj: Vec3, context: construct.Container, path: str) -> list[float]:
+        return list(obj)
+
+
+Vec3Tuple = Vec3TupleAdapter()
+
+PrimitiveConstruct = construct.Aligned(
     4,
     construct.Struct(
         type=EnumAdapter(GXPrimitive),
@@ -134,12 +251,55 @@ Border = construct.Aligned(
     ),
 )
 
+PrimitiveTableConstruct = construct.Struct(
+    index=construct.Computed(lambda this: this._index),
+    primitives=construct.PrefixedArray(
+        construct.Int32ub,
+        PrimitiveConstruct,
+    ),
+    borders=construct.PrefixedArray(
+        construct.Int32ub,
+        Border,
+    ),
+)
+
+
+class PrimitiveOffsetAdapter(construct.Adapter):
+    def __init__(self):
+        super().__init__(construct.Int32ub)
+
+    def _decode(self, obj: int, context: construct.Container, path: str) -> int:
+        end_of_header = context._.header._end_of_header
+        end_of_mappables = context._._end_of_mappable_objects
+
+        return (obj + end_of_header) - end_of_mappables
+
+    def _encode(self, obj: int, context: construct.Container, path: str) -> int:
+        end_of_header = context._.header._end_of_header
+        end_of_mappables = context._._end_of_mappable_objects
+
+        return (obj + end_of_mappables) - end_of_header
+
+
+PrimitiveOffset = PrimitiveOffsetAdapter()
+
+PrimitiveHeaderConstruct = construct.Struct(
+    normal=Vec3Tuple,
+    center_of_mass=Vec3Tuple,
+    _raw_primitive_start=construct.Peek(construct.Int32ub),
+    primitive_table_start=PrimitiveOffset,
+    _raw_border_start=construct.Peek(construct.Int32ub),
+    border_table_start=PrimitiveOffset,
+)
+
 MAPA = construct.Aligned(
     32,
     construct.Struct(
-        header=construct.Struct(
-            _magic=_const(0xDEADD00D),
-            version=construct.Switch(
+        "header"
+        / construct.Struct(
+            "_magic" / _const(0xDEADD00D),
+            "version"
+            / construct.Switch(
                 get_current_game,
                 {
                     Game.PRIME: _const(2),
@@ -148,44 +308,42 @@ MAPA = construct.Aligned(
                 },
                 default=ErrorWithMessage("Unknown game"),
             ),
-            type=construct.Int32ub,  # Light/Dark world for Echoes
-            visibility_mode=construct.Enum(construct.Int32ub, AreaVisibilty),
-            bounding_box=AABox,
-            map_adjustment=current_game_at_least_else(Game.ECHOES, Vector3, construct.Pass),
-            mappable_object_count=construct.Int32ub,
-            vertex_count=construct.Int32ub,
-            primitive_count=construct.Int32ub,
+            "type" / construct.Int32ub,  # Light/Dark world for Echoes
+            "visibility_mode" / EnumAdapter(AreaVisibility),
+            "bounding_box" / AABox,
+            "map_adjustment" / current_game_at_least_else(Game.ECHOES, Vec3Tuple, construct.Pass),
+            "_mappable_object_offset" / construct.Tell,
+            construct.Seek(4, whence=1),
+            "vertex_count" / construct.Int32ub,
+            "primitive_count" / construct.Int32ub,
+            "_end_of_header" / construct.Tell,
         ),
-        mappable_objects=construct.Array(construct.this.header.mappable_object_count, MappableObject),
-        vertices=construct.Array(construct.this.header.vertex_count, Vector3),
-        primitive_headers=construct.Array(
-            construct.this.header.primitive_count,
-            construct.Struct(
-                normal=Vector3,
-                center_of_mass=Vector3,
-                primitive_table_start=construct.Int32ub,
-                border_table_start=construct.Int32ub,
+        "mappable_objects"
+        / construct.PrefixedArray(
+            construct.Pointer(
+                lambda this: this._.header._mappable_object_offset,
+                construct.Int32ub,
             ),
+            MappableObjectConstruct,
         ),
-        primitive_tables=construct.Array(
+        "_end_of_mappable_objects" / construct.Tell,
+        "vertices" / construct.Array(construct.this.header.vertex_count, Vec3Tuple),
+        "primitive_headers"
+        / construct.Array(
             construct.this.header.primitive_count,
-            construct.Struct(
-                primitives=construct.PrefixedArray(
-                    construct.Int32ub,
-                    Primitive,
-                ),
-                borders=construct.PrefixedArray(
-                    construct.Int32ub,
-                    Border,
-                ),
-            ),
+            PrimitiveHeaderConstruct,
+        ),
+        "primitive_tables"
+        / construct.Array(
+            construct.this.header.primitive_count,
+            PrimitiveTableConstruct,
         ),
     ),
     b"\xff",
 )
 
 
-class Mapa(BaseResource):
+class Mapa[MappableObjT: (ObjectTypeMP1, ObjectTypeMP2)](BaseResource):
     @classmethod
     def construct_class(cls, target_game: Game) -> construct.Construct:
         return MAPA
@@ -196,3 +354,55 @@ class Mapa(BaseResource):
 
     def dependencies_for(self) -> typing.Iterator[Dependency]:
         yield from []
+
+    @property
+    def version(self) -> int:
+        return self.raw.header.version
+
+    @property
+    def is_dark_world(self) -> bool:
+        return self.raw.header.type == 1
+
+    @is_dark_world.setter
+    def is_dark_world(self, value: bool) -> None:
+        self.raw.header.type = 1 if value else 0
+
+    @property
+    def visibility_mode(self) -> AreaVisibility:
+        return self.raw.header.visibility_mode
+
+    @visibility_mode.setter
+    def visibility_mode(self, value: AreaVisibility) -> None:
+        self.raw.header.visibility_mode = value
+
+    @property
+    def bounding_box_min(self) -> Vec3:
+        return tuple(self.raw.header.bounding_box.min)
+
+    @bounding_box_min.setter
+    def bounding_box_min(self, value: Vec3):
+        self.raw.header.bounding_box.min = value
+
+    @property
+    def bounding_box_max(self) -> Vec3:
+        return tuple(self.raw.header.bounding_box.max)
+
+    @bounding_box_max.setter
+    def bounding_box_max(self, value: Vec3):
+        self.raw.header.bounding_box.max = value
+
+    @property
+    def map_adjustment(self) -> Vec3:
+        return tuple(self.raw.header.map_adjustment)
+
+    @map_adjustment.setter
+    def map_adjustment(self, value: Vec3):
+        self.raw.header.map_adjustment = value
+
+    @property
+    def mappable_objects(self) -> list[MappableObject[MappableObjT]]:
+        return self.raw.mappable_objects
+
+    @mappable_objects.setter
+    def mappable_objects(self, value: list[MappableObject[MappableObjT]]) -> None:
+        self.raw.mappable_objects = value
---
+++ tests/formats/test_mapa.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import pytest
+from tests import test_lib
+
+from retro_data_structures.formats.mapa import AreaVisibility, Mapa, MappableObject, ObjectTypeMP2, ObjectVisibility
+from retro_data_structures.game_check import Game
+
+# Area Map
+
+
+def test_compare_p2(prime2_asset_manager):
+    # Worlds/SwampWorld/!SwampWorld_Master/00_save_swamp_b.MAPA
+
+    test_lib.parse_and_build_compare(
+        prime2_asset_manager,
+        0xE2C3A0E5,
+        Mapa,
+    )
+
+
+def test_mapa_api_p2(prime2_asset_manager):
+    # Worlds/SwampWorld/!SwampWorld_Master/00_save_swamp_b.MAPA
+
+    mapa = prime2_asset_manager.get_parsed_asset(0xE2C3A0E5, type_hint=Mapa)
+
+    # read initial values
+    assert mapa.version == 3
+    assert not mapa.is_dark_world
+    assert mapa.visibility_mode is AreaVisibility.VisitOrMapStation
+    assert mapa.bounding_box_min == (-13.0, -9.883729934692383, 0.0)
+    assert mapa.bounding_box_max == (9.8837308883667, 9.883729934692383, 7.2974982261657715)
+    assert mapa.map_adjustment == (0.0, 0.0, -200.0)
+    assert len(mapa.mappable_objects) == 2
+
+    # set new values
+    with pytest.raises(AttributeError, match="property 'version' of 'Mapa' object has no setter"):
+        mapa.version = 1
+    mapa.is_dark_world = True
+    mapa.visibility_mode = AreaVisibility.Always
+    mapa.bounding_box_min = (1.0, 2.0, 3.0)
+    mapa.bounding_box_max = (3.0, 2.0, 1.0)
+    mapa.map_adjustment = (10.0, 20.0, 30.0)
+    mapa.mappable_objects = []
+
+    # test new values
+    assert mapa.version == 3
+    assert mapa.is_dark_world
+    assert mapa.visibility_mode is AreaVisibility.Always
+    assert mapa.bounding_box_min == (1.0, 2.0, 3.0)
+    assert mapa.bounding_box_max == (3.0, 2.0, 1.0)
+    assert mapa.map_adjustment == (10.0, 20.0, 30.0)
+    assert mapa.mappable_objects == []
+
+
+def test_add_mappable_object_p2(prime2_asset_manager):
+    # Worlds2/Temple/!Temple_World_Hub/cooked/04_temple_unknownenemies.MAPA
+
+    mapa = prime2_asset_manager.get_parsed_asset(0x4A8B769C, type_hint=Mapa)
+    mappable_obj = MappableObject[ObjectTypeMP2].create(
+        object_type=ObjectTypeMP2.TranslatorGate,
+        visibility_mode=ObjectVisibility.AreaVisitOrMapStation,
+        editor_id=0x00000000,
+        transform=[1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+    )
+    mapa.mappable_objects.append(mappable_obj)
+
+    encoded = mapa.build()
+
+    decoded = Mapa.parse(encoded, Game.ECHOES)
+
+    assert len(decoded.mappable_objects) == 4
+    assert decoded.mappable_objects[3] == mappable_obj
+
+    for orig, edited in zip(mapa.raw.primitive_headers, decoded.raw.primitive_headers, strict=True):
+        assert orig._raw_primitive_start == edited._raw_primitive_start - 0x50
+        assert orig._raw_border_start == edited._raw_border_start - 0x50
+
+        assert orig.primitive_table_start == edited.primitive_table_start
+        assert orig.border_table_start == edited.border_table_start
